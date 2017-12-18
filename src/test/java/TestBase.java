7b6c62504b4b3f6d02acb5076e03e71fbb449025
vsilver <valeriysilvanovich@gmail.com>
2017-12-18 15:45:57 +0200

    loginTest

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,4 +1,6 @@
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,15 +11,19 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
 
 import javax.security.auth.login.Configuration;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+@RunWith(JUnit4.class)
 public class TestBase {
     public WebDriver driver;
+
 
     @Before
     public void setUp() {
@@ -33,8 +39,28 @@ public class TestBase {
 
     @Test
     public void loginTest() {
-        driver.get("https://hotelfm.ru/kazan/");
-        driver.findElement(By.name("Войти")).click();
+        driver.get("https://10:10@hotelfm.ru/kazan/");
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/a[1]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        String handle = driver.getWindowHandle();
+        driver.switchTo().window(handle);
+        WebElement mylogin = wait.until(ExpectedConditions
+                .elementToBeClickable(By.xpath("//*[@id=\"AuthService_username_top\"]")));
+        mylogin.sendKeys("9172664845");
+        WebElement mypassword = wait.until(ExpectedConditions
+                .elementToBeClickable(By.xpath("//*[@id=\"AuthService_username_top\"]")));
+        mypassword.sendKeys("12345");
+        driver.findElement(By.xpath("//*[@id=\"AuthService_password\"]"))
+                .sendKeys("12345");
+        driver.findElement(By.xpath("//*[@id=\"log_button_top\"]")).click();
+        WebElement mytitle = wait.until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath("/html/head/title")));
+        Assert.assertEquals("kazan",driver.getTitle());
+    }
+
+    @Test
+    public void failLoginTest() {
+
 
     }
 
